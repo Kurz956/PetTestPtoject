b74c07616a76c05fcc846bdb5c44da0728fee13f
Kurz <kurz956kk@gmail.com>
2024-03-26 13:29:32 +0500

    login_page 1st full working stage completed

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,14 +11,15 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         current_url = self.driver.current_url
-        assert 'login' in current_url, f'"login" is not in current url: {current_url}'
+        login = 'login'
+        assert login in current_url, f'"{login}" is not in current url: {current_url}'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
         form_login = self.is_element_present(*LoginPageLocators.FORM_LOGIN)
-        assert False, "Can't find form_login"   # поменять на form_login
+        assert form_login, "Can't find form_login"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         form_registration = self.is_element_present(*LoginPageLocators.FORM_REGISTRATION)
-        assert False, "Can't find form_registration"  # поменять на form_registration
+        assert form_registration, "Can't find form_registration"
--- test_main_page.py
+++ test_main_page.py
@@ -6,10 +6,10 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
-    link_login = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
     page = MainPage(driver, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
+    login_page = LoginPage(driver=driver, url=driver.current_url)
+    login_page.should_be_login_page()
 
 
