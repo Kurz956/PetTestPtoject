667ce8f58574a3e7decc56e16ae7edc6e1a643be
Kurz <kurz956kk@gmail.com>
2024-03-26 12:06:21 +0500

    base_page and main_page first logic have been created

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
-def test_guest_can_go_to_login_page(driver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(driver):
     login_link = driver.find_element('css selector', "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(driver):
+   driver.get(link)
+   go_to_login_page(driver)
